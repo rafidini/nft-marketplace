d4f8ca23a72dec048187a957c6db9c2c34c14998
LcPlacide <loicplacide@orange.fr>
2021-10-01 21:25:36 +0200

    selenium with chrome

--- scrapers/crypto.py
+++ scrapers/crypto.py
@@ -4,9 +4,10 @@ from utils import *
 WAIT = 3*60 # 15 min = 900s
 NEXT_START = datetime.now()+timedelta(seconds=WAIT)
 start_url = 'https://www.binance.com/fr/markets'
+browser = None
 
 while True:
-    browser = open_binance(start_url, False) # ouverture de la page principale des cryptos
+    browser = open_binance(start_url, browser, handle_cookies=False) # ouverture de la page principale des cryptos
     nb_pages = find_crypto_pages(browser) # recherche de pages additionnelles
     parse_crypto_pages(browser, nb_pages) # scraping des pages de cryptos
-    NEXT_START = wait_next_scraping(NEXT_START, WAIT) # attente avant prochain scraping 
+    NEXT_START = wait_next_scraping(NEXT_START, WAIT) # attente avant prochain scraping
--- scrapers/nft.py
+++ scrapers/nft.py
@@ -4,10 +4,12 @@ from utils import *
 WAIT = 3*60 # 5h = 18000s
 MAX_NFT_NB = 16
 NEXT_START = datetime.now()+timedelta(seconds=WAIT)
+start_url = 'https://www.binance.com/en/nft/market?currency=&mediaType=&tradeType=&amountFrom=&amountTo=&categorys=&keyword=&page=1&rows=16&productIds=&order=list_time%40-1'
+browser= None
 
 while True:
-    browser = open_marketplace() # ouverture du marketplace
+    browser = open_binance(start_url, browser) # ouverture du marketplace
     scroll_down(browser, MAX_NFT_NB) # scroll down sur marketplace
-    detailed_pages = find_detailed_pages(browser, MAX_NFT_NB) # recherche de pages détaillées
-    parse_detailed_pages(browser, detailed_pages) # scraping des pages détaillées
+    detailed_pages = find_detailed_nft_pages(browser, MAX_NFT_NB) # recherche de pages détaillées
+    parse_detailed_nft_pages(browser, detailed_pages) # scraping des pages détaillées
     NEXT_START = wait_next_scraping(NEXT_START, WAIT) # attente avant prochain scraping 
